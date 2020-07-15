8be5f03c1aec9fc8941ddaa33a9f71197a5df084
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-16 02:25:20 +0300

    feat: remove debug log

--- src/controllers/start.controller.ts
+++ src/controllers/start.controller.ts
@@ -1,6 +1,5 @@
 import { Context } from 'telegraf';
 
 export const StartController = async (ctx: Context): Promise<void> => {
-  console.log(ctx.update.message?.from);
   ctx.reply(`${new Date().toLocaleString()} - start`);
 };
