fec975e8bc5869a9ac8fb5f793f7bfb540cca905
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-16 16:11:13 +0300

    feat: connect help controller and service

--- src/controllers/help.controller.ts
+++ src/controllers/help.controller.ts
@@ -1,6 +1,7 @@
-import { Context } from 'telegraf';
+import { TextContext } from '../modules/bot/interfaces';
+import { HelpService } from '../services';
 
-export const HelpController = async (ctx: Context): Promise<void> => {
+export const HelpController = async (ctx: TextContext): Promise<void> => {
   const helpText = `
 /add {rank} - добавляет новое звание. 
 /update {rankId} {new_rank} - меняет значение звания (например, если ошибся в слове). 
@@ -10,5 +11,9 @@ export const HelpController = async (ctx: Context): Promise<void> => {
 /unassign {rankId} {username} - переносит присвоенное звание обратно в список доступных (например, если ошибся в присваивании).
 /help - показывает справку.
   `;
-  ctx.reply(helpText);
+
+  const service = new HelpService({ text: helpText });
+  const response = service.handle();
+
+  await ctx.reply(response.text);
 };
