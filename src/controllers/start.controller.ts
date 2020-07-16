254798dcae17ca10451ee1c2faeb845ccfca9429
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-16 16:12:59 +0300

    feat: update start controller response

--- src/controllers/start.controller.ts
+++ src/controllers/start.controller.ts
@@ -1,5 +1,5 @@
 import { Context } from 'telegraf';
 
 export const StartController = async (ctx: Context): Promise<void> => {
-  ctx.reply(`${new Date().toLocaleString()} - start`);
+  await ctx.reply('Здорова, бандиты');
 };
