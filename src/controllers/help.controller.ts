020e811fd7057add555233861864a242f872222d
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-16 14:33:15 +0300

    feat: add help text

--- src/controllers/help.controller.ts
+++ src/controllers/help.controller.ts
@@ -1,5 +1,14 @@
 import { Context } from 'telegraf';
 
 export const HelpController = async (ctx: Context): Promise<void> => {
-  ctx.reply('/help not implemented');
+  const helpText = `
+/add {rank} - добавляет новое звание. 
+/update {rankId} {new_rank} - меняет значение звания (например, если ошибся в слове). 
+/delete {rankId} - удалить звание из общего списка.  
+/list - вывести список всех званий по пользователям и общий. 
+/assign {rankId} {username} {comment} - присваивает звание кому-то из участников чата. Последним аргументом можно дописать комментарий, когда и как было присвоено звание.
+/unassign {rankId} {username} - переносит присвоенное звание обратно в список доступных (например, если ошибся в присваивании).
+/help - показывает справку.
+  `;
+  ctx.reply(helpText);
 };
