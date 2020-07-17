b1a1c87907cd84a02d15b8b8de22ecf3b08d4768
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-17 23:40:55 +0300

    feat: connect add controller and service

--- src/controllers/add.controller.ts
+++ src/controllers/add.controller.ts
@@ -1,5 +1,17 @@
-import { Context } from 'telegraf';
+import { TextContext } from '../modules/bot/interfaces';
+import { AddService } from '../services';
+import { RankDao } from '../modules/db/dao';
 
-export const AddController = async (ctx: Context): Promise<void> => {
-  ctx.reply('/add not implemented');
+export const AddController = async (ctx: TextContext): Promise<void> => {
+  const rankTitle = ctx.update.message.text.slice(
+    ctx.update.message.entities[0].length + 1,
+    ctx.update.message.text.length,
+  );
+  const rankDao = new RankDao();
+  const service = new AddService({
+    rank: { title: rankTitle },
+    dao: { rank: rankDao },
+  });
+  const response = await service.handle();
+  ctx.reply(response.text);
 };
