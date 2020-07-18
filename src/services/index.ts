c2360595deb7e6ba3e87ebb6d1d96d31b1330483
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-18 04:04:08 +0300

    feat: add update service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,12 @@ import { HelpService } from './help.service';
 import { ListService } from './list.service';
 import { AddService } from './add.service';
 import { DeleteService } from './delete.service';
+import { UpdateService } from './update.service';
 
 export {
   HelpService,
   ListService,
   AddService,
   DeleteService,
+  UpdateService,
 };
---
+++ src/services/update.service.ts
@@ -0,0 +1,53 @@
+import { UpdateResponse } from '../responses';
+import { RankDao, ChangelogDao } from '../modules/db/dao';
+
+interface UpdateServiceData {
+  user: { id: number };
+  rank: { id: number; nextTitle: string };
+  dao: { rank: RankDao; changelog: ChangelogDao };
+}
+
+export class UpdateService {
+  private dao: { rank: RankDao; changelog: ChangelogDao };
+
+  private rank: { id: number; nextTitle: string };
+
+  private user: { id: number };
+
+  public constructor(data: UpdateServiceData) {
+    this.rank = data.rank;
+    this.dao = data.dao;
+    this.user = data.user;
+  }
+
+  public async handle(): Promise<UpdateResponse> {
+    if (Number.isNaN(this.rank.id) === true) {
+      return { text: 'Нет такого звания, пошел нахуй, долбаеб' };
+    }
+
+    const rank = await this.dao.rank.getRank({ id: this.rank.id });
+
+    if (rank === null) {
+      return { text: 'Нет такого звания, пошел нахуй, долбаеб' };
+    }
+
+    await this.dao.rank.updateRank({ id: rank.id, title: this.rank.nextTitle });
+
+    await this.dao.changelog.createChangelog({
+      userId: this.user.id,
+      type: 'update',
+      table: 'ranks',
+      objectId: rank.id,
+      previousValue: rank.title,
+      currentValue: this.rank.nextTitle,
+    });
+
+    const response = new UpdateResponse({
+      rankId: rank.id,
+      rankPreviousTitle: rank.title,
+      rankCurrentTitle: this.rank.nextTitle,
+    });
+
+    return response;
+  }
+}
