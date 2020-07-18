277ea2d6e87f9c56026f29673a58c41224dc867c
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-18 03:19:34 +0300

    feat: add changelog dao

---
+++ src/modules/db/dao/changelog.dao.ts
@@ -0,0 +1,42 @@
+import { Repository } from 'typeorm';
+import { DbModule } from '../db.module';
+import { ChangelogEntity } from '../entities';
+
+interface CreateChangelogData {
+  userId: number;
+  table: string;
+  type: string;
+  objectId: number;
+  previousValue?: string;
+  currentValue?: string;
+}
+
+export class ChangelogDao {
+  private repository: Repository<ChangelogEntity>;
+
+  public constructor() {
+    this.repository = DbModule.getRepository(ChangelogEntity);
+  }
+
+  public async getChangelogs(): Promise<ChangelogEntity[]> {
+    const changelogs = await this.repository.find();
+    return changelogs;
+  }
+
+  public async createChangelog(
+    data: CreateChangelogData,
+  ): Promise<void> {
+    const {
+      type, userId, table, objectId, previousValue, currentValue,
+    } = data;
+
+    await this.repository.save({
+      type,
+      table,
+      objectId,
+      user: { id: userId },
+      previousValue,
+      currentValue,
+    });
+  }
+}
--- src/modules/db/dao/index.ts
+++ src/modules/db/dao/index.ts
@@ -1,9 +1,11 @@
 import { RankDao } from './rank.dao';
 import { UserDao } from './user.dao';
 import { RankToUserDao } from './rank-to-user.dao';
+import { ChangelogDao } from './changelog.dao';
 
 export {
   RankDao,
   UserDao,
   RankToUserDao,
+  ChangelogDao,
 };
