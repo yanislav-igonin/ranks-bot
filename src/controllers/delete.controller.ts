8f43be0203bcfdb3a4dccbc2a11375f720c4f8e7
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-18 03:50:23 +0300

    feat: connect delete service to controller

--- src/controllers/delete.controller.ts
+++ src/controllers/delete.controller.ts
@@ -1,5 +1,23 @@
-import { Context } from 'telegraf';
+import { TextContext } from '../modules/bot/interfaces';
+import { DeleteService } from '../services';
+import { RankDao, ChangelogDao } from '../modules/db/dao';
 
-export const DeleteController = async (ctx: Context): Promise<void> => {
-  ctx.reply('/delete not implemented');
+export const DeleteController = async (ctx: TextContext): Promise<void> => {
+  const rankText = ctx.update.message.text.slice(
+    ctx.update.message.entities[0].length + 1,
+    ctx.update.message.text.length,
+  );
+  const rankId = parseInt(rankText, 10);
+
+  const rankDao = new RankDao();
+  const changelogDao = new ChangelogDao();
+
+  const service = new DeleteService({
+    user: { id: ctx.update.message.from.id },
+    rank: { id: rankId },
+    dao: { rank: rankDao, changelog: changelogDao },
+  });
+
+  const response = await service.handle();
+  ctx.reply(response.text);
 };
