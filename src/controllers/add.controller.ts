0810b434c9b83d10deb1cef6b7f2143045ce643d
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-18 03:32:57 +0300

    feat: add changelog save on rank add

--- src/controllers/add.controller.ts
+++ src/controllers/add.controller.ts
@@ -1,17 +1,22 @@
 import { TextContext } from '../modules/bot/interfaces';
 import { AddService } from '../services';
-import { RankDao } from '../modules/db/dao';
+import { RankDao, ChangelogDao } from '../modules/db/dao';
 
 export const AddController = async (ctx: TextContext): Promise<void> => {
   const rankTitle = ctx.update.message.text.slice(
     ctx.update.message.entities[0].length + 1,
     ctx.update.message.text.length,
   );
+
   const rankDao = new RankDao();
+  const changelogDao = new ChangelogDao();
+
   const service = new AddService({
+    user: { id: ctx.update.message.from.id },
     rank: { title: rankTitle },
-    dao: { rank: rankDao },
+    dao: { rank: rankDao, changelog: changelogDao },
   });
+
   const response = await service.handle();
   ctx.reply(response.text);
 };
--- src/services/add.service.ts
+++ src/services/add.service.ts
@@ -1,23 +1,35 @@
 import { AddResponse } from '../responses';
-import { RankDao } from '../modules/db/dao';
+import { RankDao, ChangelogDao } from '../modules/db/dao';
 
 interface AddServiceData {
+  user: { id: number };
   rank: { title: string };
-  dao: { rank: RankDao };
+  dao: { rank: RankDao; changelog: ChangelogDao };
 }
 
 export class AddService {
-  private dao: { rank: RankDao };
+  private dao: { rank: RankDao; changelog: ChangelogDao };
 
   private rank: { title: string };
+
+  private user: { id: number };
 
   public constructor(data: AddServiceData) {
     this.rank = data.rank;
     this.dao = data.dao;
+    this.user = data.user;
   }
 
   public async handle(): Promise<AddResponse> {
     const rank = await this.dao.rank.createRank({ title: this.rank.title });
+
+    await this.dao.changelog.createChangelog({
+      userId: this.user.id,
+      type: 'update',
+      table: 'ranks',
+      objectId: rank.id,
+    });
+
     const response = new AddResponse({ rankId: rank.id, rankTitle: rank.title });
 
     return response;
