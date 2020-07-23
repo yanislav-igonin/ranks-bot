303bf5f9d912d7b64fa802c949f81e0ab7c5397a
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-23 20:51:08 +0300

    feat: update unassign response

--- src/responses/unassign.response.ts
+++ src/responses/unassign.response.ts
@@ -2,13 +2,21 @@ interface UnassignResponseData {
   username: string;
   rankId: number;
   rankTitle: string;
+  rankCount?: number;
 }
 
 export class UnassignResponse {
   public text: string;
 
   public constructor(data: UnassignResponseData) {
-    const { rankId, rankTitle, username } = data;
-    this.text = `Аннулировано звание @${username}: ${rankTitle}, ID - ${rankId}`;
+    const {
+      rankId, rankTitle, username, rankCount,
+    } = data;
+
+    if (rankCount) {
+      this.text = `Повторно аннулировано звание @${username}: ${rankTitle}, счетчик - ${rankCount}, ID - ${rankId}`;
+    } else {
+      this.text = `Аннулировано звание @${username}: ${rankTitle}, ID - ${rankId}`;
+    }
   }
 }
