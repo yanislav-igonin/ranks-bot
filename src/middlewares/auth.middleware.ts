88de7bb72907b5837c62f616108e90070098c2a5
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-24 22:54:17 +0300

    feat: remove ctx check in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -6,11 +6,9 @@ export const AuthMiddleware: Middleware<TextContext> = async (
   ctx,
   next,
 ): Promise<void> => {
-  if (ctx.update.message !== undefined && ctx.update.message.from !== undefined) {
-    if (!AuthConfig.users.includes(ctx.update.message.from.id)) {
-      await ctx.reply('Соси бибу, пес');
-      return;
-    }
-    await next();
+  if (!AuthConfig.users.includes(ctx.update.message.from.id)) {
+    await ctx.reply('Соси бибу, пес');
+    return;
   }
+  await next();
 };
