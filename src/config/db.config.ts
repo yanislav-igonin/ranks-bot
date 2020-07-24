dee017f2cf03fd3afd5f83f458d16392f59d1fa7
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-24 20:49:02 +0300

    feat: add db sync env var

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,9 +1,7 @@
-import { AppConfig } from './app.config';
-
 const DbConfig = {
   url: process.env.DB_URL || 'localhost',
   // logging: AppConfig.env === 'development',
-  synchronize: AppConfig.env === 'development',
+  synchronize: process.env.DB_SYNC === 'true',
 };
 
 export { DbConfig };
