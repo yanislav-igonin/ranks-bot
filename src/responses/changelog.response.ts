4911d3dfd9dbf52d159a946bdd1e9f358eb5ed49
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-30 02:10:53 +0300

    feat: changelog response wip

--- src/responses/changelog.response.ts
+++ src/responses/changelog.response.ts
@@ -1,21 +1,58 @@
-interface ChangelogChunk {
-  createdAt: string;
-  type: string;
-  table: string;
+export interface ChangelogChunk {
   previousValue: string;
   currentValue: string;
 }
 
 interface ChangelogResponseData {
-  changelog: ChangelogChunk[];
+  [K: string]: {
+    added?: ChangelogChunk[];
+    deleted?: ChangelogChunk[];
+    updated?: ChangelogChunk[];
+    assigned?: ChangelogChunk[];
+    unassigned?: ChangelogChunk[];
+  };
 }
 
 export class ChangelogResponse {
   public text: string;
 
   public constructor(data: ChangelogResponseData) {
-    const { changelog } = data;
+    this.text = 'Лог изменений\n\n';
 
-    this.text = 'Лог изменений\n';
+    const dates = Object.keys(data);
+
+    for (const date of dates) {
+      this.text = this.text.concat(`${date}\n`);
+
+      const changesForDate = data[date];
+
+      if (changesForDate.added) {
+        this.text = this.text.concat('Добавлено\n');
+
+        for (const addedElem of changesForDate.added) {
+          this.text = this.text.concat(`${addedElem.currentValue}\n`);
+        }
+      }
+
+      if (changesForDate.deleted) {
+        this.text = this.text.concat('Удалено\n');
+
+        for (const deletedElem of changesForDate.deleted) {
+          this.text = this.text.concat(`${deletedElem.previousValue}\n`);
+        }
+      }
+
+
+      this.text = this.text.concat('\n');
+    }
   }
 }
+
+/*
+29.07.2020
+Добавлено:
+Удалено
+Изменено:
+Присвоено:
+Аннулировано:
+*/
