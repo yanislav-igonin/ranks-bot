8ea2ec3513e18d0bc3d874401666a0ea45fd3bc3
Yanislav Igonin <yanislavdabest@gmail.com>
2020-07-30 18:16:57 +0300

    feat: finish response view

--- src/responses/changelog.response.ts
+++ src/responses/changelog.response.ts
@@ -27,32 +27,62 @@ export class ChangelogResponse {
       const changesForDate = data[date];
 
       if (changesForDate.added) {
-        this.text = this.text.concat('Добавлено\n');
+        this.text = this.text.concat('Добавлено:\n');
 
         for (const addedElem of changesForDate.added) {
           this.text = this.text.concat(`${addedElem.currentValue}\n`);
         }
+
+        this.text = this.text.concat('\n');
+      }
+
+      if (changesForDate.updated) {
+        this.text = this.text.concat('Изменено:\n');
+
+        for (const updatedElem of changesForDate.updated) {
+          this.text = this.text
+            .concat(`${updatedElem.previousValue} -> ${updatedElem.currentValue}\n`);
+        }
+
+        this.text = this.text.concat('\n');
       }
 
       if (changesForDate.deleted) {
-        this.text = this.text.concat('Удалено\n');
+        this.text = this.text.concat('Удалено:\n');
 
         for (const deletedElem of changesForDate.deleted) {
           this.text = this.text.concat(`${deletedElem.previousValue}\n`);
         }
+
+        this.text = this.text.concat('\n');
       }
 
+      if (changesForDate.assigned) {
+        this.text = this.text.concat('Присвоено:\n');
 
-      this.text = this.text.concat('\n');
+        for (const assignedElem of changesForDate.assigned) {
+          this.text = this.text.concat(`${assignedElem.currentValue}\n`);
+        }
+
+        this.text = this.text.concat('\n');
+      }
+
+      if (changesForDate.unassigned) {
+        this.text = this.text.concat('Аннулировано:\n');
+
+        for (const unassignedElem of changesForDate.unassigned) {
+          if (unassignedElem.currentValue === '') {
+            this.text = this.text.concat(`${unassignedElem.previousValue}\n`);
+          } else {
+            this.text = this.text
+              .concat(`${unassignedElem.previousValue} -> ${unassignedElem.currentValue}\n`);
+          }
+        }
+
+        this.text = this.text.concat('\n');
+      }
+
+      this.text = this.text.concat('\n\n');
     }
   }
 }
-
-/*
-29.07.2020
-Добавлено:
-Удалено
-Изменено:
-Присвоено:
-Аннулировано:
-*/
